65fbdef9dba405d65a25599b04834539709db3f4
KaisarOrange <alif.ayodya@gmail.com>
2023-01-23 22:35:10 +0700

    commit

--- components/Project.jsx
+++ components/Project.jsx
@@ -8,6 +8,7 @@ function Project() {
   const [selected, setSelect] = useState(0);
   const image = [
     '/img/bangkitT.mp4',
+    '/img/boi.mp4',
     '/img/pasta.mp4',
     '/img/calc.mp4',
     '/img/tic.mp4',
@@ -15,24 +16,29 @@ function Project() {
   const title = ['  '];
   const link = [
     'https://kaisarorange.github.io/bangkit-app/',
+    'https://github.com/KaisarOrange/order-app',
     'https://pastaboyss.web.app/',
     'https://kaisarorange.github.io/calculator/',
     'https://kaisarorange.github.io/tic-tac-toe/',
+    'https://kaisarorange.github.io/tic-tac-toe/',
   ];
   const deskripti = [
-    ' University final project web application',
+    'University final project web application',
+    'Order App',
     'Landing page for a food pre order business',
     'Web application based calculator',
     'Tic Tac Toe game project',
   ];
   const git = [
     'https://github.com/KaisarOrange/bangkit-app',
+    'https://github.com/KaisarOrange/order-app',
     '',
     'https://github.com/KaisarOrange/calculator',
     'https://github.com/KaisarOrange/tic-tac-toe',
   ];
   const stack = [
     ['React.js', 'Chakra UI', 'Javascript', 'React Query', 'Firebase'],
+    ['Angular', 'Tailwind', 'Typescript'],
     ['React', 'Tailwind', 'Javascript'],
     ['HTML', 'CSS', 'Javascript'],
     ['HTML', 'CSS', 'Javascript'],
@@ -62,7 +68,7 @@ function Project() {
                   selected === 1 ? styles.selected : ''
                 }`}
               ></div>
-              <div className={styles.text}>Food business landing page</div>
+              <div className={styles.text}>Order App</div>
             </div>
             <div onClick={() => setSelect(2)} className={styles.sidebar}>
               {' '}
@@ -71,13 +77,22 @@ function Project() {
                   selected === 2 ? styles.selected : ''
                 }`}
               ></div>
-              <div className={styles.text}>Calculator</div>
+              <div className={styles.text}>Food business landing page</div>
             </div>
             <div onClick={() => setSelect(3)} className={styles.sidebar}>
               {' '}
               <div
                 className={`${styles.sidebarBar} ${
                   selected === 3 ? styles.selected : ''
+                }`}
+              ></div>
+              <div className={styles.text}>Calculator</div>
+            </div>
+            <div onClick={() => setSelect(4)} className={styles.sidebar}>
+              {' '}
+              <div
+                className={`${styles.sidebarBar} ${
+                  selected === 4 ? styles.selected : ''
                 }`}
               ></div>
               <div className={styles.text}>Tic Tac Toe</div>
