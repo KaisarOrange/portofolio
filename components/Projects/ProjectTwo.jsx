32f03b96aa220676f18ecfb0eb3a9580f49dbd93
KaisarOrange <alif.ayodya@gmail.com>
2023-06-09 19:22:26 +0700

    added project contents

--- components/Projects/ProjectTwo.jsx
+++ components/Projects/ProjectTwo.jsx
@@ -15,6 +15,21 @@ function ProjectTwo() {
     { name: 'PostgreSql', image: './img/postgres.png' },
     { name: 'Tailwind', image: './img/tail.svg' },
   ];
+  const parasatuPasta = [
+    'Pastaboys is a small business located in Malang, Indonesia. Being run by dormary students, Pastaboys hold dearly products quality and customer satistfaction.',
+  ];
+
+  const parasatuAdmin = [
+    'To handle orders, pastaboys uses a sophisticated admin dashboard. node js with express.js framework for backend and passport.js with Session strategy to authenticate its api routes and to access  the application.',
+  ];
+  const paraDuaPasta = [
+    'Pastaboys App Minimum Viable Product (MVP) is currently powered with Angular and Firebase as Backend.',
+  ];
+
+  const paraDuaAdmin = [''];
+  const paraTigaPasta = [
+    'Switching from using SaaS to a standalone robust Node Express backend server with PostgreSql as database',
+  ];
   const itemsAdmin = [
     { name: 'React', image: './img/react.svg' },
     { name: 'React query', image: './img/rq.png' },
@@ -72,12 +87,20 @@ function ProjectTwo() {
       <Pastaboys
         title={'Pastaboys'}
         images={imagesPasta}
+        parasatu={parasatuPasta}
+        indev={true}
+        paradua={paraDuaPasta}
+        paratiga={paraTigaPasta}
         width={300}
         heigth={600}
         items={items}
       />
       <Pastaboys
         title={'Admin Dashboard'}
+        parasatu={parasatuAdmin}
+        paradua={paraDuaAdmin}
+        paratiga={''}
+        indev={false}
         images={imagesAdmin}
         width={700}
         heigth={350}
--- components/Projects/components/Pastaboys.jsx
+++ components/Projects/components/Pastaboys.jsx
@@ -4,36 +4,37 @@ import SwipeableTextMobileStepper from '../Carousel';
 import { Box, Typography } from '@mui/material';
 import TechnologyParser from './TechnologyParser';
 
-function Pastaboys({ title, images, width, heigth, items }) {
+function Pastaboys({
+  title,
+  images,
+  width,
+  heigth,
+  items,
+  parasatu,
+  paradua,
+  paratiga,
+  indev,
+}) {
   return (
     <Box
       height={'100vh'}
       display={'flex'}
+      gap={'20px'}
       justifyContent={'space-between'}
       alignItems={'center'}
     >
-      <Box width={'30%'}>
+      <Box>
         <Typography sx={{ fontSize: '3.2rem', fontWeight: 'bold' }}>
           {title}
         </Typography>
 
-        <Typography sx={{ marginTop: '20px' }}>
-          Pastaboys is a small business located in Malang, Indonesia. Being run
-          by dormary students, Pastaboys hold dearly products quality and
-          customer satistfaction.
-        </Typography>
-        <Typography sx={{ marginTop: '20px' }}>
-          Pastaboys App Minimum Viable Product (MVP) is currently powered with
-          Angular and Firebase as Backend.
-        </Typography>
+        <Typography sx={{ marginTop: '20px' }}>{parasatu}</Typography>
+        <Typography sx={{ marginTop: '20px' }}>{paradua}</Typography>
 
         <Typography sx={{ marginTop: '40px', fontWeight: 'bold' }}>
-          In Development:
+          {indev ? 'In Development' : ''}
         </Typography>
-        <Typography sx={{ marginTop: '0px' }}>
-          Switching from using SaaS to a standalone robust Node Express backend
-          server with PostgreSql as database
-        </Typography>
+        <Typography sx={{ marginTop: '0px' }}>{paratiga}</Typography>
         <Typography
           sx={{ marginTop: '40px', marginBottom: '10px', fontWeight: 'bold' }}
         >
