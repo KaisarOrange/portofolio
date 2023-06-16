31538f75146761abe3095fb69dfcc9d176dbcee3
KaisarOrange <alif.ayodya@gmail.com>
2023-06-16 08:00:53 +0700

    changed layout

--- components/Projects/ProjectTwo.jsx
+++ components/Projects/ProjectTwo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SwipeableTextMobileStepper from './Carousel';
 import { Box, Typography } from '@mui/material';
-
+import { useRouter } from 'next/router';
 import styles from '../../styles/Projects.module.scss';
 import { useInView } from 'react-intersection-observer';
 import Pastaboys from './components/Pastaboys';
@@ -10,7 +10,6 @@ import Admin from './components/Admin';
 function ProjectTwo() {
   const items = [
     { name: 'Angular', image: './img/angular.png' },
-    { name: 'Firebase', image: './img/firebase.png' },
     { name: 'Express.js', image: './img/express.png' },
     { name: 'PostgreSql', image: './img/postgres.png' },
     { name: 'Tailwind', image: './img/tail.svg' },
@@ -22,9 +21,11 @@ function ProjectTwo() {
   const parasatuAdmin = [
     'To handle orders, pastaboys uses a sophisticated admin dashboard. node js with express.js framework for backend and passport.js with Session strategy to authenticate its api routes and to access  the application.',
   ];
-  const paraDuaPasta = [
-    'Pastaboys App Minimum Viable Product (MVP) is currently powered with Angular and Firebase as Backend.',
+  const parasatuBangkit = [
+    'Bangkit is a final semester university project, Bangkit main ambition was to help small business who were affected by covid 19. Bangkit is built with the idea of gotong royong, Investors who are interested are able to invest their money in great businesses',
   ];
+
+  const paraDuaPasta = [''];
 
   const paraDuaAdmin = [''];
   const paraTigaPasta = [
@@ -37,6 +38,12 @@ function ProjectTwo() {
     { name: 'PostgreSql', image: './img/postgres.png' },
     { name: 'Material UI', image: './img/mui.png' },
     { name: 'Passport.js', image: './img/ps.webp' },
+  ];
+  const itemsBangkit = [
+    { name: 'React', image: './img/react.svg' },
+    { name: 'React query', image: './img/rq.png' },
+    { name: 'Chakra UI', image: './img/chakra.png' },
+    { name: 'Firebase', image: './img/firebase.png' },
   ];
   const imagesAdmin = [
     {
@@ -74,6 +81,32 @@ function ProjectTwo() {
       imgPath: '/img/ee.png',
     },
   ];
+  const imagesBangkit = [
+    {
+      label: 'Finish Page',
+      imgPath: '/img/Bangkit/5.png',
+    },
+    {
+      label: 'Menu Page',
+      imgPath: '/img/Bangkit/1.png',
+    },
+    {
+      label: 'OrderPage',
+      imgPath: '/img/Bangkit/2.png',
+    },
+    {
+      label: 'Finish Page',
+      imgPath: '/img/Bangkit/3.png',
+    },
+    {
+      label: 'Finish Page',
+      imgPath: '/img/Bangkit/4.png',
+    },
+    {
+      label: 'Finish Page',
+      imgPath: '/img/Bangkit/6.png',
+    },
+  ];
 
   const { ref, inView } = useInView({ threshold: 0, triggerOnce: true });
   return (
@@ -102,8 +135,11 @@ function ProjectTwo() {
         width={300}
         heigth={600}
         items={items}
+        live={'https://pastaboys.shop'}
+        github={'https://github.com/KaisarOrange/order-app'}
+        liveEnabled={true}
       />
-      <Box sx={{ margin: { xs: 30, md: 15 } }}></Box>
+      <Box sx={{ margin: { xs: 15, md: 15 } }}></Box>
       <Pastaboys
         title={'Admin Dashboard'}
         parasatu={parasatuAdmin}
@@ -114,6 +150,24 @@ function ProjectTwo() {
         width={700}
         heigth={350}
         items={itemsAdmin}
+        liveEnabled={false}
+        live={''}
+        github={'https://github.com/KaisarOrange/admin'}
+      />
+      <Box sx={{ margin: { xs: 15, md: 15 } }}></Box>
+      <Pastaboys
+        title={'Bangkit'}
+        parasatu={parasatuBangkit}
+        paradua={paraDuaAdmin}
+        paratiga={''}
+        indev={false}
+        images={imagesBangkit}
+        width={700}
+        heigth={350}
+        liveEnabled={true}
+        items={itemsBangkit}
+        live={'https://kaisarorange.github.io/bangkit-app/'}
+        github={'https://github.com/KaisarOrange/bangkit-app'}
       />
     </Box>
   );
--- components/Projects/components/Pastaboys.jsx
+++ components/Projects/components/Pastaboys.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import GitHubIcon from '@mui/icons-material/GitHub';
 import SwipeableTextMobileStepper from '../Carousel';
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import TechnologyParser from './TechnologyParser';
+
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function Pastaboys({
   title,
@@ -15,14 +18,18 @@ function Pastaboys({
   paratiga,
   indev,
   isPhone,
+  github,
+  live,
+  liveEnabled,
 }) {
+  const router = useRouter();
   return (
     <Box
       height={'100vh'}
       display={'flex'}
       sx={{
         flexDirection: { xs: 'column', md: isPhone ? 'row' : 'column' },
-        alignItems: { xs: '', md: 'center' },
+        alignItems: { xs: '', md: '' },
       }}
       marginTop={{ xs: 10, md: 0 }}
       gap={'20px'}
@@ -41,10 +48,32 @@ function Pastaboys({
         <Typography sx={{ marginTop: '20px' }}>{parasatu}</Typography>
         <Typography sx={{ marginTop: '20px' }}>{paradua}</Typography>
 
-        <Typography sx={{ marginTop: '40px', fontWeight: 'bold' }}>
+        {/* <Typography sx={{ marginTop: '40px', fontWeight: 'bold' }}>
           {indev ? 'In Development' : ''}
         </Typography>
-        <Typography sx={{ marginTop: '0px' }}>{paratiga}</Typography>
+        <Typography sx={{ marginTop: '0px' }}>{paratiga}</Typography> */}
+
+        <Button
+          variant='contained'
+          sx={{ margin: '5px', backgroundColor: '#000057' }}
+          disabled={!liveEnabled}
+          onClick={() => {
+            window.open(live);
+          }}
+        >
+          Live Demo
+        </Button>
+
+        <Button
+          variant='outlined'
+          onClick={() => {
+            window.open(github);
+          }}
+          sx={{ margin: '5px', borderColor: '#000057', color: '#000057' }}
+        >
+          Github <GitHubIcon style={{ marginLeft: '5px' }}></GitHubIcon>
+        </Button>
+
         <Typography
           sx={{ marginTop: '40px', marginBottom: '10px', fontWeight: 'bold' }}
         >
